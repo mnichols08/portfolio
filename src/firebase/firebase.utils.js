c9c0b76325abd8900905e35b2c4142d9735a1c3e
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-14 06:35:02 -0400

    create firebase firestore utils file

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+
+
+const config = {
+    apiKey: "AIzaSyC2UYAuLIAIFzF5mH17r_gxU0-LqK2T9p8",
+    authDomain: "mnix-dev.firebaseapp.com",
+    projectId: "mnix-dev",
+    storageBucket: "mnix-dev.appspot.com",
+    messagingSenderId: "739467354682",
+    appId: "1:739467354682:web:f9a8b75a867d8228a98b03",
+    measurementId: "G-J4XD88RM63"
+  };
+
+  const app = initializeApp(config);
+  const db = getFirestore(app);
+  const getCollection = async (db, _collection) => {
+    const array = collection(db, _collection);
+    const snapshot = await getDocs(array);
+    const list = snapshot.docs.map(doc => doc.data());
+    return list;
+  }
+
+  export const projects = getCollection(db, 'projects');
