58898b02b0ee5c21a01383708032191f4800b4ea
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-14 13:18:16 -0400

    creates a Delete Project functionality from Portfolio Index

--- src/components/Portfolio/Index.component.js
+++ src/components/Portfolio/Index.component.js
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react';
-import { collection, query, onSnapshot } from "firebase/firestore";
+import { collection, doc, updateDoc, deleteDoc, onSnapshot } from "firebase/firestore";
 import { db } from '../../firebase/firebase.utils';
 
 function Portfolio() {
  const [projects, setProjects] = useState([]);
+ const handleEdit = e => {
+  e.preventDefault()
+  console.log(arguments)
+ }
+
   useEffect(() => {
     const ref = collection(db, 'projects')
     onSnapshot(ref, (snapshot) => {    
@@ -18,13 +23,26 @@ function Portfolio() {
     return (
       <ul>
       <u>Projects: </u>
-      {projects.map(project => (
-        <li key={project.id}>
-        <h3><a href={`/portfolio/${project.id}`}>{project.data.title}</a></h3>
-        <p>{project.data.desc}</p>
-        <p>Languages:  { project.data.lang }</p>
-        </li>
-      ))}
+      {projects.map(project => {
+        const handleDelete = async e => {
+          e.preventDefault();
+          const ref = doc(db, 'projects', project.id)
+          try{
+            await deleteDoc(ref)
+          } catch (err) {
+            alert(err)
+          }
+        }
+        return (
+          <li key={project.id}>
+          <h3><a href={`/portfolio/${project.id}`}>{project.data.title}</a><a href={`/portfolio/${project.id}/edit`} onClick={handleEdit}>/</a>
+          <a href={`/portfolio/${project.id}/delete`} onClick={handleDelete}>x</a></h3>
+          <p>{project.data.desc}</p>
+          <p>Languages:  { project.data.lang }</p>
+          
+          </li>
+        )
+      })}
       </ul>
     );
   }
