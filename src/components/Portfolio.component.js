8c17a65b517603cf366b786a7ad47b6785b500ec
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-14 07:51:23 -0400

    import projects from firebase into Portfolio component

--- src/components/Portfolio.component.js
+++ src/components/Portfolio.component.js
@@ -1,13 +1,29 @@
 import { useEffect, useState } from 'react';
-
+import { collection, query, onSnapshot } from "firebase/firestore";
+import { db } from '../firebase/firebase.utils';
 function Portfolio() {
-  const [projects, setProjects] = useState([]);
+ const [projects, setProjects] = useState([]);
   useEffect(() => {
+    const ref = collection(db, 'projects')
+    onSnapshot(ref, (snapshot) => {    
+      setProjects(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data() 
+        }))
+      );
+    });
   }, [ ]);
     return (
-      <div>
-
-      </div>
+      <ul>
+      <u>Projects: </u>
+      {projects.map(project => (
+        <li>
+        <h3>{project.title}</h3>
+        <p>{project.desc}</p>
+        </li>
+      ))}
+      </ul>
     );
   }
   
