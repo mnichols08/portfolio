90c42139864e74780fa88f6a5c2784a863684eed
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 15:25:30 -0400

    create Certs Component

---
+++ src/components/Welcome/Resume/Certs.component.js
@@ -0,0 +1,17 @@
+import './certs.styles.css';
+function Certs() {
+    return (
+        <ul className="certs">
+        <h3>Certifications</h3>
+        <li>Google Analytics</li>
+        <li>Google Ads</li>
+        <li>BigCommerce Partnership</li>
+        <li>Mountain Bike Guiding</li>
+        <li>PSIA - Cross Country Skiing</li>
+        <li>AASI - Snowboarding</li>
+        <li>Swift Water Rescue</li>
+        <li>First Aid / CPR</li>
+      </ul>
+    )
+}
+export default Certs;
