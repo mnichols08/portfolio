0af5df103136c7391a9071674a1fe186291a269f
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 15:29:06 -0400

    create Skillset Component

---
+++ src/components/Welcome/Resume/Skillset.component.js
@@ -0,0 +1,34 @@
+import './skillset.styles.css';
+function Skillset(){
+    return (
+        <section className="skills" id="skills">
+              <hr />
+              <h3>My Skillset Includes</h3>
+                <ul>
+                  <li>React</li>
+                  <li>Redux</li>
+                  <li>Tabletop.js</li>
+                  <li>AutoIT</li>
+                  <li>BigCommerce</li>
+                  <li>BootStrap</li>
+                  <li>CSS3</li>
+                  <li>Google Ads</li>
+                  <li>Google Analytics</li>
+                  <li>Google App Script</li>
+                  <li>HTML5</li>
+                  <li>JS</li>
+                  <li>LESS</li>
+                  <li>Magento 1</li>
+                  <li>NCR CounterPoint</li>
+                  <li>PHP</li>
+                  <li>SASS</li>
+                  <li>SQL</li>
+                  <li>Search Engine Optimization</li>
+                  <li>WordPress</li>
+                  <li>eCommerce Consulting</li>
+                  <li>jQuery</li>
+                </ul>     
+          </section>  
+    )
+}
+export default Skillset
