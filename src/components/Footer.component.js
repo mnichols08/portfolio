20d57b5914ba72f75ef2dda5a54424962123263d
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-15 00:45:00 -0400

    add copyright to Footer Component

--- src/components/Footer.component.js
+++ src/components/Footer.component.js
@@ -1,14 +1,16 @@
 function Footer() {
-    return (
-      <footer className='watermark'>
+  return (
+    <footer className="watermark">
       <p>
-      <a href="https://github.com/mnichols08/" target="_blank">
-    <i class="fas fa-code text-green"></i> with <i class="fas fa-heart text-red"></i> by <i class="fab fa-codepen text-yellow">mnichols08</i>
-  </a>
-</p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
-  
+        <a href="https://github.com/mnichols08/" target="_blank">
+          <i class="fas fa-code text-green"></i> with{" "}
+          <i class="fas fa-heart text-red"></i> by{" "}
+          <i class="fab fa-codepen text-yellow">mnichols08</i>
+        </a>
+        &nbsp; Copyright &copy; {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
