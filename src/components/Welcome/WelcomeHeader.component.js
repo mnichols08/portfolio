7b5b9fa60de560c20ef100efe1f31fdbf9510121
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 12:13:43 -0400

    create WelcomeHeader Component

---
+++ src/components/Welcome/WelcomeHeader.component.js
@@ -0,0 +1,14 @@
+import './header.styles.css'
+function WelcomeHeader() {
+    return (
+
+  <div className="welcome-header">
+    <h1>Mikey Nichols</h1>
+    <h2>Full Stack Web Developer</h2>
+  </div>
+  
+
+    );
+  }
+  
+  export default WelcomeHeader;
