cea0a9dbe9e872229b74b1a350377816049fd2c4
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 15:27:11 -0400

    create Extracurricular Component

---
+++ src/components/Welcome/Resume/Extracurricular.component.js
@@ -0,0 +1,30 @@
+import './extracurricular.styles.css'
+function Extracurricular(){
+    return(
+        <section className="extras" id="extras">
+              <h3>Extracurricular</h3>
+                <ul>
+                    <div>
+                      <h4>Udemy</h4>
+                      <h5>Advanced CSS &amp; SASS by Jonas Schmedtmann</h5>
+                    </div>
+                    <div>
+                      <h4>February 2019 - Present</h4>
+                      <h5>Online, Part Time</h5>
+                    </div>
+                  </ul>
+                  <ul>
+                      <div>
+                        <h4>Udemy</h4>
+                        <h5>Complete JavaScript by Jonas Schmedtmann</h5>
+                      </div>
+                      <div>
+                        <h4>November 2018 - January 2019</h4>
+                        <h5>Online, Part-Time</h5>
+                      </div>
+                    </ul>
+        </section>
+    )
+}
+
+export default Extracurricular;
