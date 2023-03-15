413dd8184cf5d04fdac375bccd71c639c4717162
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 12:56:48 -0400

    import Resume Component into Welcome Component

--- src/components/Welcome/Index.component.js
+++ src/components/Welcome/Index.component.js
@@ -1,9 +1,12 @@
-import WelcomeHeader from './WelcomeHeader.component'
+import WelcomeHeader from './WelcomeHeader.component';
+import Resume from './Resume.component';
+
 function Welcome() {
     return (
 
  <div>
   <WelcomeHeader />
+  <Resume />
  </div>
   
 
