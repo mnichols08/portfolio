4245f32003336a683bed157c972c4cd70c82b01f
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 17:26:48 -0400

    changes class to className

--- src/components/Footer.component.js
+++ src/components/Footer.component.js
@@ -3,9 +3,9 @@ function Footer() {
     <footer className="watermark">
       <p>
         <a href="https://github.com/mnichols08/" target="_blank">
-          <i class="fas fa-code text-green"></i> with{" "}
-          <i class="fas fa-heart text-red"></i> by{" "}
-          <i class="fab fa-codepen text-yellow">mnichols08</i>
+          <i className="fas fa-code text-green"></i> with{" "}
+          <i className="fas fa-heart text-red"></i> by{" "}
+          <i className="fab fa-codepen text-yellow">mnichols08</i>
         </a>
         &nbsp; Copyright &copy; {new Date().getFullYear()}
       </p>
