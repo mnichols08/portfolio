6b4cd13bd567fcdeb8886e231f659fd2431fdbf2
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 15:26:30 -0400

    create Education Component

---
+++ src/components/Welcome/Resume/Education.component.js
@@ -0,0 +1,29 @@
+import './education.styles.css';
+function Education(){
+    return(
+        <section className="education" id="education">
+          <h3>Education</h3>
+          <ul>
+              <div>
+                <h4>Lambda School</h4>
+                <h5>Full Stack Web Development</h5>
+              </div>
+              <div>
+                <h4>May 2019 - Present</h4>
+                <h5>Online, Part-Time</h5>
+              </div>
+            </ul>
+            <ul>
+                <div>
+                  <h4>Adventure Sports Institute</h4>
+                  <h5>A.A.S - Adventure Sports Management</h5>
+                </div>
+                <div>
+                  <h4>January 2014 - January 2016</h4>
+                  <h5>McHenry, MD</h5>
+                </div>
+              </ul>
+          </section>
+    )
+}
+export default Education;
