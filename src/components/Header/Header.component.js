b9fbe34b28cc6ec8870a3c5d8bc05e93c61e70bb
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 18:16:12 -0400

    link to resume page

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -5,7 +5,7 @@ function Header() {
         <nav>
           <ul>
             <li><a href="/">Home</a></li>
-            <li><a href="#resume">Resume</a></li>
+            <li><a href="/resume">Resume</a></li>
             <li><a href="/portfolio">Portfolio</a></li>
             <li><a href="/gallery">Photos</a></li>
             <li><a href="/blog">Blog</a></li>
