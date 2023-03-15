75c8fab488dd5c80c70941b01f70ebe72458de57
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-14 23:50:40 -0400

    Import Footer into App Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Header from './components/Header.component'
+import Header from './components/Header.component';
+import Footer from './components/Footer.component';
 function App({Element}) {
   return (
     <div className="App">
       <Header />
       <Element />
+      <Footer />
     </div>
   );
 }
