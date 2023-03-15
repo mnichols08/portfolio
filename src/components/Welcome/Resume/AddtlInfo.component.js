ed096264148e0d8b170a1cf3a5fd1aa78572b9b1
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 15:25:11 -0400

    create AddtlInfo Component

---
+++ src/components/Welcome/Resume/AddtlInfo.component.js
@@ -0,0 +1,11 @@
+import './addtl-info.styles.css';
+function AddtlInfo(){
+    return(
+        <ul className="addtl-info">
+            <h3>Additional Information</h3>
+            <li>Languages: English, Spanish, Thai</li>
+            <hr />
+        </ul> 
+    )
+}
+export default AddtlInfo;
