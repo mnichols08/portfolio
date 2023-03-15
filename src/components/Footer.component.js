721cf2befb34f101c5bf9feadcea778e7124f8a0
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-14 23:48:37 -0400

    create Footer Component

---
+++ src/components/Footer.component.js
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+      <footer className='watermark'>
+      <p>
+      <a href="https://github.com/mnichols08/" target="_blank">
+    <i class="fas fa-code text-green"></i> with <i class="fas fa-heart text-red"></i> by <i class="fab fa-codepen text-yellow">mnichols08</i>
+  </a>
+</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
