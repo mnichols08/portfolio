f37482a4f6ce5f1288969c92b26f00fdd488785f
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 16:44:27 -0400

    creates same functionality for skills to sort by length as in my static webpage version of this same thing

--- src/components/Welcome/Resume/LeftSide/Skillset/Skillset.component.js
+++ src/components/Welcome/Resume/LeftSide/Skillset/Skillset.component.js
@@ -1,32 +1,12 @@
 import './skillset.styles.css';
 function Skillset(){
+  const skillset = ['React', 'Redux', 'Tabletop.js', 'AutoIT', 'BigCommerce', 'BootStrap', 'CSS3', 'Google Ads', 'Google Analytics', 'Google App Script', 'HTML5', 'JS', 'LESS', 'Magento 1', 'NCR CounterPoint', 'PHP', 'SASS', 'SQL', 'Search Engine Optimization', 'WordPress', 'eCommerce Consulting', 'jQuery'].sort((a,b) => b.length - a.length);
     return (
         <section className="skills" id="skills">
               <hr />
               <h3>My Skillset Includes</h3>
                 <ul>
-                  <li>React</li>
-                  <li>Redux</li>
-                  <li>Tabletop.js</li>
-                  <li>AutoIT</li>
-                  <li>BigCommerce</li>
-                  <li>BootStrap</li>
-                  <li>CSS3</li>
-                  <li>Google Ads</li>
-                  <li>Google Analytics</li>
-                  <li>Google App Script</li>
-                  <li>HTML5</li>
-                  <li>JS</li>
-                  <li>LESS</li>
-                  <li>Magento 1</li>
-                  <li>NCR CounterPoint</li>
-                  <li>PHP</li>
-                  <li>SASS</li>
-                  <li>SQL</li>
-                  <li>Search Engine Optimization</li>
-                  <li>WordPress</li>
-                  <li>eCommerce Consulting</li>
-                  <li>jQuery</li>
+                  {skillset.map(skill => <li>{skill}</li>)}
                 </ul>     
           </section>  
     )
