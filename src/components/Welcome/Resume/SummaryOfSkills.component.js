e05959d24b581c357bdc70516b8044264bbfe51e
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 15:29:23 -0400

    create SummaryOfSkills Component

---
+++ src/components/Welcome/Resume/SummaryOfSkills.component.js
@@ -0,0 +1,27 @@
+import './summary-of-skills.styles.css';
+function SummaryOfSkills(){
+    return (
+        <section className="intro" id="intro">
+            <h3>Summary of Skills</h3>
+            <ul>
+              <h4>Communication</h4>
+              <li>Professional and courteous writing skills</li>
+              <li>Convey messages through words and visual aids</li>
+              <li>Merits based on written communication skills</li>
+            </ul>
+            <ul>
+                <h4>Analytics</h4>
+                <li>Foresight to predict problems before they occur</li>
+                <li>Ability to track trends through reports</li>
+                <li>Focused and proven track record for growth</li>
+            </ul>
+            <ul>
+                <h4>Programming</h4>
+                <li>I take pride in solving problems with code</li>
+                <li>Currently focuses on the fundamentals of JavaScript</li>
+                <li>Strong desire to learn and excel at the task at hand</li>
+            </ul>
+          </section>
+    )
+}
+export default SummaryOfSkills;
