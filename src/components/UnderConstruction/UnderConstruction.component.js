ecd6653cbecb145b5d367ee2d642afdfe44661d4
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 17:08:27 -0400

    create Under Construction Component

---
+++ src/components/UnderConstruction/UnderConstruction.component.js
@@ -0,0 +1,11 @@
+function UnderConstruction(){
+    return (
+        <div className="under-construction">
+            <h1>Under Construction</h1>
+            <p>Sorry but the page you have requested is under construction.</p>
+            <p>Please come back again later.</p>
+            <img src="/img/underconstruction.gif" alt="under construction"/>
+        </div>
+    )
+}
+export default UnderConstruction
