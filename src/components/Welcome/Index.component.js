f0ffbc2b1871298987f5b7ca904029a8e734367e
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 18:16:48 -0400

    remove Resume Component from Welcome Component

--- src/components/Welcome/Index.component.js
+++ src/components/Welcome/Index.component.js
@@ -1,12 +1,10 @@
 import WelcomeHeader from './WelcomeHeader.component';
-import Resume from '../Resume/Resume.component';
 
 function Welcome() {
     return (
 
  <div>
   <WelcomeHeader />
-  <Resume />
  </div>
   
 
