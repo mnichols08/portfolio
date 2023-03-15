8c8c417c24c6f246e0f7a0def60ca6b3571ee572
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-15 00:57:03 -0400

    add skills to Welcome Component

--- src/components/Welcome/Index.component.js
+++ src/components/Welcome/Index.component.js
@@ -4,6 +4,20 @@ function Welcome() {
       <div className="welcome">
         <h1>Hi There, I am Mikey,</h1>
         <p>a <span className="pro">pro</span>grammer.</p>
+        <hr />
+        <div className="skills">
+          <h2>My Skills.</h2>
+          <div className="skill-row">
+            <img className="" src="" alt="" />
+            <h3>JavaScript</h3>
+            <p>I can write full stack web applications by leveraging Node.js to create backends for many scenarios</p>
+          </div>
+          <div className="skill-row">
+            <img className="" src="" alt="" />
+            <h3>SCRUM & AGILE Ready</h3>
+            <p>First hand experience with SCRUM having worked in an Agile environment with Chingu.</p>
+          </div>
+        </div>
       </div>
     );
   }
