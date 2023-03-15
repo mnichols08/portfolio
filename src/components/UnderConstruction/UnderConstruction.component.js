cf694de28241be5d72b500deabfee5e301c3eefe
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-15 19:58:41 -0400

    import stylesheet into Under Construction Component

--- src/components/UnderConstruction/UnderConstruction.component.js
+++ src/components/UnderConstruction/UnderConstruction.component.js
@@ -1,10 +1,11 @@
+import './under-construction.styles.css';
 function UnderConstruction(){
     return (
         <div className="under-construction">
             <h1>Under Construction</h1>
             <p>Sorry but the page you have requested is under construction.</p>
-            <p>Please come back again later.</p>
             <img src="/img/underconstruction.gif" alt="under construction"/>
+            <p>Please come back again later.</p>
         </div>
     )
 }
