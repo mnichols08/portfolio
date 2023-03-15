a28ad18893cf5c6d0bf5069d746b878c8d51076c
Mikey Nichols <mikenichols410@gmail.com>
2023-03-15 12:16:56 -0400

    convert html into a custom WelcomeHeader component

--- src/components/Welcome/Index.component.js
+++ src/components/Welcome/Index.component.js
@@ -1,24 +1,12 @@
-import './index.styles.css'
+import WelcomeHeader from './WelcomeHeader.component'
 function Welcome() {
     return (
-      <div className="welcome">
-        <h1>Hi There, I am Mikey,</h1>
-        <p>a <span className="pro">pro</span>grammer.</p>
-        <hr />
-        <div className="skills">
-          <h2>My Skills.</h2>
-          <div className="skill-row">
-            <img className="" src="" alt="" />
-            <h3>JavaScript</h3>
-            <p>I can write full stack web applications by leveraging Node.js to create backends for many scenarios</p>
-          </div>
-          <div className="skill-row">
-            <img className="" src="" alt="" />
-            <h3>SCRUM & AGILE Ready</h3>
-            <p>First hand experience with SCRUM having worked in an Agile environment with Chingu.</p>
-          </div>
-        </div>
-      </div>
+
+ <div>
+  <WelcomeHeader />
+ </div>
+  
+
     );
   }
   
