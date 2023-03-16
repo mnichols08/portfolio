67206962ca0b6410917e22b3803eae7ca05cd268
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-16 02:00:36 -0400

    passes in a key to each ul being created by map

--- src/components/Resume/RightSide/SummaryOfSkills/SummaryOfSkills.component.js
+++ src/components/Resume/RightSide/SummaryOfSkills/SummaryOfSkills.component.js
@@ -6,8 +6,8 @@ function SummaryOfSkills({sumOfSkills}){
         <section className="intro" id="intro">
             <h3>Summary of Skills</h3>
             {
-                Object.keys(skills).map(skill => (
-                    <ul>
+                Object.keys(skills).map((skill,y) => (
+                    <ul key={`skill_${y}`}>
                     <h4>{ skill }</h4>
                     {skills[skill].map((item, i) => (
                         <li key={`item_${i}`}>{item}</li>
