3bed9b8145eaf74c7d0ca13f2b81f19369839426
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-16 02:19:20 -0400

    converts static education data into dynamic

--- src/components/Resume/RightSide/Education/Education.component.js
+++ src/components/Resume/RightSide/Education/Education.component.js
@@ -1,28 +1,22 @@
 import './education.styles.css';
-function Education(){
+function Education({education}){
+  let knowledge
+  education ? knowledge = education : knowledge = [];
     return(
         <section className="education" id="education">
           <h3>Education</h3>
-          <ul>
+          {knowledge.map((school, i) => (
+            <ul key={`school_${i}`}>
               <div>
-                <h4>Lambda School</h4>
-                <h5>Full Stack Web Development</h5>
+                <h4>{school.academy}</h4>
+                <h5>{school.field_of_study}</h5>
               </div>
               <div>
-                <h4>May 2019 - Present</h4>
-                <h5>Online, Part-Time</h5>
+              <h4>{school.dates}</h4>
+              <h5>{school.location}</h5>
               </div>
             </ul>
-            <ul>
-                <div>
-                  <h4>Adventure Sports Institute</h4>
-                  <h5>A.A.S - Adventure Sports Management</h5>
-                </div>
-                <div>
-                  <h4>January 2014 - January 2016</h4>
-                  <h5>McHenry, MD</h5>
-                </div>
-              </ul>
+          ))}
           </section>
     )
 }
