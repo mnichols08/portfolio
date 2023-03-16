520136e7d5c6cf1cc5613263618d9c627f4ec714
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-16 00:43:21 -0400

    different approach to mapping skills array

--- src/components/Resume/LeftSide/Skillset/Skillset.component.js
+++ src/components/Resume/LeftSide/Skillset/Skillset.component.js
@@ -1,15 +1,13 @@
 import './skillset.styles.css';
 function Skillset({skillset}){
-  try {
-  skillset = skillset.sort((a,b) => b.length - a.length);
-  } catch {
-  }
-    return (
+  let skills
+  skillset ? skills = skillset.sort((a,b) => b.length - a.length) : skills = [];
+     return (
         <section className="skills" id="skills">
               <hr />
               <h3>My Skillset Includes</h3>
                 <ul>
-                  {skillset.map((skill, i) => <li key={i}>{skill}</li>)}
+                  {skills.map((skill, i) => <li key={i}>{skill}</li>)}
                 </ul>     
           </section>  
     )
