42dc3f1a0c8cdd1da665716585401473dab43d39
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-16 01:58:39 -0400

    converts static html content to dynamic

--- src/components/Resume/RightSide/SummaryOfSkills/SummaryOfSkills.component.js
+++ src/components/Resume/RightSide/SummaryOfSkills/SummaryOfSkills.component.js
@@ -1,26 +1,20 @@
 import './summary-of-skills.styles.css';
-function SummaryOfSkills(){
+function SummaryOfSkills({sumOfSkills}){
+    let skills
+    sumOfSkills ? skills = sumOfSkills : skills = [];
     return (
         <section className="intro" id="intro">
             <h3>Summary of Skills</h3>
-            <ul>
-              <h4>Communication</h4>
-              <li>Professional and courteous writing skills</li>
-              <li>Convey messages through words and visual aids</li>
-              <li>Merits based on written communication skills</li>
-            </ul>
-            <ul>
-                <h4>Analytics</h4>
-                <li>Foresight to predict problems before they occur</li>
-                <li>Ability to track trends through reports</li>
-                <li>Focused and proven track record for growth</li>
-            </ul>
-            <ul>
-                <h4>Programming</h4>
-                <li>I take pride in solving problems with code</li>
-                <li>Currently focuses on the fundamentals of JavaScript</li>
-                <li>Strong desire to learn and excel at the task at hand</li>
-            </ul>
+            {
+                Object.keys(skills).map(skill => (
+                    <ul>
+                    <h4>{ skill }</h4>
+                    {skills[skill].map((item, i) => (
+                        <li key={`item_${i}`}>{item}</li>
+                    ))}
+                    </ul>
+                ))
+            }
           </section>
     )
 }
