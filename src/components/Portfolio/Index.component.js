d09c9878f6079cfe585e175edef879fbd46a109d
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-15 20:26:36 -0400

    restructure Portfolio Component for stylesheet and import said stylesheet

--- src/components/Portfolio/Index.component.js
+++ src/components/Portfolio/Index.component.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { collection, onSnapshot } from "firebase/firestore";
 import { db } from '../../firebase/firebase.utils';
+import './index.styles.css'
 
 function Portfolio() {
  const [projects, setProjects] = useState([]);
@@ -17,18 +18,21 @@ function Portfolio() {
     });
   }, [ ]);
     return (
-      <ul>
-      <u>Projects: </u>
-      {projects.map(project => {
-        return (
-          <li key={project.id}>
-          <h3><a href={`/portfolio/${project.id}`}>{project.data.title} </a> </h3>
-          <p>{project.data.desc}</p>
-          <p>Languages:  { project.data.lang }</p>
-          </li>
-        )
-      })}
-      </ul>
+      <div className="portfolio">
+      <h1><u>Projects: </u></h1>
+        <ul>
+        {projects.map(project => {
+          return (
+            <li key={project.id}>
+            <h3><a href={`/portfolio/${project.id}`}>{project.data.title} </a> </h3>
+            <p>{project.data.desc}</p>
+            <p>Languages:  { project.data.lang }</p>
+            </li>
+          )
+        })}
+        </ul>
+      </div>
+   
     );
   }
   
