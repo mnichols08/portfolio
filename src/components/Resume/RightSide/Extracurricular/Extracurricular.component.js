14b31eb76e8fa526983c6570e5ce16ec4939c138
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-16 02:26:50 -0400

    updates static text in Extracurricular Component to dynamic

--- src/components/Resume/RightSide/Extracurricular/Extracurricular.component.js
+++ src/components/Resume/RightSide/Extracurricular/Extracurricular.component.js
@@ -1,28 +1,22 @@
 import './extracurricular.styles.css'
-function Extracurricular(){
+function Extracurricular({extra}){
+  let extras
+  extra ? extras = extra : extras = [];
     return(
         <section className="extras" id="extras">
               <h3>Extracurricular</h3>
-                <ul>
-                    <div>
-                      <h4>Udemy</h4>
-                      <h5>Advanced CSS &amp; SASS by Jonas Schmedtmann</h5>
-                    </div>
-                    <div>
-                      <h4>February 2019 - Present</h4>
-                      <h5>Online, Part Time</h5>
-                    </div>
-                  </ul>
-                  <ul>
-                      <div>
-                        <h4>Udemy</h4>
-                        <h5>Complete JavaScript by Jonas Schmedtmann</h5>
-                      </div>
-                      <div>
-                        <h4>November 2018 - January 2019</h4>
-                        <h5>Online, Part-Time</h5>
-                      </div>
-                    </ul>
+              { extras.map((activity, i) => (
+                <ul key={`activity_${i}`}>
+                <div>
+                  <h4>{activity.origin}</h4>
+                  <h5>{activity.title}</h5>
+                </div>
+                <div>
+                  <h4>{activity.dates}</h4>
+                  <h5>{activity.location}</h5>
+                </div>
+                </ul>
+              ))}
         </section>
     )
 }
