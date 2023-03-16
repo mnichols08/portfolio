b76c1b98606fce081b6f2535a3787e3f43645c2c
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-16 00:23:19 -0400

    carries in skillset props from Left Side Component

--- src/components/Resume/LeftSide/Skillset/Skillset.component.js
+++ src/components/Resume/LeftSide/Skillset/Skillset.component.js
@@ -1,6 +1,9 @@
 import './skillset.styles.css';
-function Skillset(){
-  const skillset = ['React', 'Redux', 'Tabletop.js', 'AutoIT', 'BigCommerce', 'BootStrap', 'CSS3', 'Google Ads', 'Google Analytics', 'Google App Script', 'HTML5', 'JS', 'LESS', 'Magento 1', 'NCR CounterPoint', 'PHP', 'SASS', 'SQL', 'Search Engine Optimization', 'WordPress', 'eCommerce Consulting', 'jQuery'].sort((a,b) => b.length - a.length);
+function Skillset({skillset}){
+  try {
+  skillset = skillset.sort((a,b) => b.length - a.length);
+  } catch {
+  }
     return (
         <section className="skills" id="skills">
               <hr />
