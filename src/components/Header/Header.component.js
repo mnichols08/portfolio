290182a40a71f549965c4c8bfc793cb2b7b1e455
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-15 20:00:14 -0400

    create link to /contact in Header Nav Comoponent

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -9,7 +9,7 @@ function Header() {
             <li><a href="/portfolio">Portfolio</a></li>
             <li><a href="/gallery">Photos</a></li>
             <li><a href="/blog">Blog</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="/contact">Contact</a></li>
           </ul>
         </nav>
       </header>
