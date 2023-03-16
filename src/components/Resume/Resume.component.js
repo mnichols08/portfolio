e772b68115694dfbd535ab4a803e84ca37bcf61e
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-16 00:18:46 -0400

    retrieves resume object from Firebase and feeds it as a property to both sides of Resume Component

--- src/components/Resume/Resume.component.js
+++ src/components/Resume/Resume.component.js
@@ -1,11 +1,19 @@
+import { useState, useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase/firebase.utils";
 import LeftSide from './LeftSide/LeftSide.component';
 import RightSide from './RightSide/RightSide.component';
 import './resume.styles.css'
 function Resume() {
+    const [resume, setResume] = useState([]);
+    const getDocSnap = async () => await getDoc(doc(db, "resume", 'resume')).then(doc => setResume(doc.data()))
+    useEffect(() => {
+      getDocSnap()
+      }, []);
     return (
         <div className="resume">
-        <LeftSide />
-        <RightSide />
+        <LeftSide resume={resume}/>
+        <RightSide resume={resume}/>
     </div>
     );
   }
