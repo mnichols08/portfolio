eceb5ba34c372f1dc3ca52b6c09330ee82f8afae
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-16 22:06:04 -0400

    converts text on landing page to dynamic text from db

--- src/components/Welcome/WelcomeHeader.component.js
+++ src/components/Welcome/WelcomeHeader.component.js
@@ -1,10 +1,18 @@
+import { useState, useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase/firebase.utils";
 import './welcome-header.styles.css'
 function WelcomeHeader() {
+  const [header, setHeader] = useState([]);
+  const getDocSnap = async () => await getDoc(doc(db, "pages", 'home')).then(doc => setHeader(doc.data()))
+  useEffect(() => {
+    getDocSnap()
+    }, []);
     return (
 
   <div className="welcome-header">
-    <h1>Mikey Nichols</h1>
-    <h2>Full Stack Web Developer</h2>
+    <h1>{header.heading}</h1>
+    <h2>{header.sub_heading}</h2>
   </div>
   
 
