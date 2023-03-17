dc2f691239ae04797164374d0a7b776aedf73670
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-16 22:01:09 -0400

    passes in a message from db to the button in Coffee Component

--- src/components/Resume/LeftSide/Coffee/Coffee.component.js
+++ src/components/Resume/LeftSide/Coffee/Coffee.component.js
@@ -1,9 +1,9 @@
 import './coffee.styles.css';
-function Coffee({email, caption}){
+function Coffee({email, caption, message}){
     return(
         <div className="coffee">
           <p>{ caption }</p>
-          <a href={`mailto:${email}?subject=Let's%20grab%20coffee!`} className="button">Let's Grab Coffee!</a>
+          <a href={`mailto:${email}?subject=Let's%20grab%20coffee!`} className="button">{ message }</a>
         </div>
     )
 }
