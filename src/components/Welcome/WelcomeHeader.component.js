bc15f9b0910314259da8ba83618c3c5185b9a18a
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-17 23:47:51 -0400

    imports the auth function from firebase to detect if the user is me or not

--- src/components/Welcome/WelcomeHeader.component.js
+++ src/components/Welcome/WelcomeHeader.component.js
@@ -1,22 +1,37 @@
 import { useState, useEffect } from "react";
 import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../firebase/firebase.utils";
-import './welcome-header.styles.css'
+import { db, auth } from "../../firebase/firebase.utils";
+import Edit from "../Edit/Edit.component";
+import "./welcome-header.styles.css";
 function WelcomeHeader() {
+  const [heading, setHeading] = useState();
+  const [editHeading, setEditHeading] = useState([]);
   const [header, setHeader] = useState([]);
-  const getDocSnap = async () => await getDoc(doc(db, "pages", 'home')).then(doc => setHeader(doc.data()))
-  useEffect(() => {
-    getDocSnap()
-    }, []);
-    return (
-
-  <div className="welcome-header">
-    <h1>{header.heading}</h1>
-    <h2>{header.sub_heading}</h2>
-  </div>
-  
-
-    );
+  let admin;
+  try {
+    let user = auth.currentUser;
+    admin = user.uid === `Mdu1H6ns4neFWEHMxLzQtwX6CTp2`;
+    console.log(auth);
+  } catch (e) {
+    console.error(e);
+    admin = false;
   }
-  
-  export default WelcomeHeader;
+  const getDocSnap = async () =>
+    await getDoc(doc(db, "pages", "home")).then((doc) => setHeader(doc.data()));
+  useEffect(() => {
+    getDocSnap();
+  }, []);
+  return (
+    <div className="welcome-header">
+      <h1>
+        {header.heading}
+        {admin ? <Edit handleClick={setEditHeading} /> : ""}
+      </h1>
+      <h2>
+        {header.sub_heading}
+        {admin ? <Edit handleClick={setEditHeading} /> : ""}
+      </h2>
+    </div>
+  );
+}
+export default WelcomeHeader;
