341fcfc5712519b2bf6c836d8ec1a1a35255df66
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-17 23:53:46 -0400

    strips logic to detect admin

--- src/components/Welcome/WelcomeHeader.component.js
+++ src/components/Welcome/WelcomeHeader.component.js
@@ -1,21 +1,12 @@
 import { useState, useEffect } from "react";
 import { doc, getDoc } from "firebase/firestore";
-import { db, auth } from "../../firebase/firebase.utils";
+import { db } from "../../firebase/firebase.utils";
 import Edit from "../Edit/Edit.component";
 import "./welcome-header.styles.css";
 function WelcomeHeader() {
   const [heading, setHeading] = useState();
   const [editHeading, setEditHeading] = useState([]);
   const [header, setHeader] = useState([]);
-  let admin;
-  try {
-    let user = auth.currentUser;
-    admin = user.uid === `Mdu1H6ns4neFWEHMxLzQtwX6CTp2`;
-    console.log(auth);
-  } catch (e) {
-    console.error(e);
-    admin = false;
-  }
   const getDocSnap = async () =>
     await getDoc(doc(db, "pages", "home")).then((doc) => setHeader(doc.data()));
   useEffect(() => {
@@ -25,11 +16,11 @@ function WelcomeHeader() {
     <div className="welcome-header">
       <h1>
         {header.heading}
-        {admin ? <Edit handleClick={setEditHeading} /> : ""}
+        <Edit handleClick={setEditHeading} />
       </h1>
       <h2>
         {header.sub_heading}
-        {admin ? <Edit handleClick={setEditHeading} /> : ""}
+        <Edit handleClick={setEditHeading} />
       </h2>
     </div>
   );
