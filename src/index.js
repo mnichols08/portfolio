c6d9abede5ba6f4b1df9b1dbc1e6276717f5f0f3
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-19 15:25:50 -0400

    import Projects component

--- src/index.js
+++ src/index.js
@@ -1,69 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './reset.styles.css'
-import './typography.styles.css';
-import './index.styles.css';
-import App from './App';
-import UnderConstruction from './components/UnderConstruction/UnderConstruction.component';
-import Welcome from './components/Welcome/Index.component';
-import Login from './components/Login/Login.component';
-import Resume from './components/Resume/Resume.component';
-import Portfolio from './components/Portfolio/Index.component';
-import AddProject from './components/Portfolio/Add.component';
-import ViewProject from './components/Portfolio/View.component';
-import Blog from './components/Blog/Index.component';
-import AddBlogPost from './components/Blog/Add.component';
-import BlogPost from './components/Blog/View.component';
-import reportWebVitals from './reportWebVitals';
+import "./reset.styles.css";
+import "./typography.styles.css";
+import "./index.styles.css";
+import App from "./App";
+import UnderConstruction from "./components/UnderConstruction/UnderConstruction.component";
+import Welcome from "./components/Welcome/Index.component";
+import Login from "./components/Login/Login.component";
+import Resume from "./components/Resume/Resume.component";
+import Portfolio from "./components/Portfolio/Index.component";
+import AddProject from "./components/Portfolio/Add.component";
+import ViewProject from "./components/Portfolio/View.component";
+import Projects from "./components/Projects/Projects.component";
+import Blog from "./components/Blog/Index.component";
+import AddBlogPost from "./components/Blog/Add.component";
+import BlogPost from "./components/Blog/View.component";
+import reportWebVitals from "./reportWebVitals";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App Element={Welcome} />,
   },
-  { path: '/login',
-    element: <App Element={Login} />
-  },
+  { path: "/projects", element: <App Element={Projects} /> },
+  { path: "/login", element: <App Element={Login} /> },
   {
     path: "/resume",
     element: <App Element={Resume} />,
   },
   {
-    path: 'portfolio/add',
-    element: <App Element={AddProject} />
+    path: "portfolio/add",
+    element: <App Element={AddProject} />,
   },
   {
-    path: 'portfolio/:id',
-    element: <App Element={ViewProject} />
+    path: "portfolio/:id",
+    element: <App Element={ViewProject} />,
   },
   {
     path: "/portfolio",
     element: <App Element={Portfolio} />,
   },
   {
-    path: '/blog/:id',
-    element: <App Element={BlogPost} />
+    path: "/blog/:id",
+    element: <App Element={BlogPost} />,
   },
   {
-    path: '/blog/add',
-    element: <App Element={AddBlogPost} />
+    path: "/blog/add",
+    element: <App Element={AddBlogPost} />,
   },
   {
     path: "/blog",
-    element: <App Element={UnderConstruction} />
+    element: <App Element={UnderConstruction} />,
   },
   {
     path: "/gallery",
-    element:  <App Element={UnderConstruction} />
+    element: <App Element={UnderConstruction} />,
   },
   {
     path: "/contact",
-    element:  <App Element={UnderConstruction} />
-  }
-])
+    element: <App Element={UnderConstruction} />,
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
