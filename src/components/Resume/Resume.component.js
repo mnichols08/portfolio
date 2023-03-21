d7298137b269c5114408287b290ff92e146021dd
Mikey Nichols <mnix@journeytocode.io>
2023-03-20 23:34:31 -0400

    sends github user information to resume components

--- src/components/Resume/Resume.component.js
+++ src/components/Resume/Resume.component.js
@@ -6,14 +6,18 @@ import RightSide from './RightSide/RightSide.component';
 import './resume.styles.css'
 function Resume() {
     const [resume, setResume] = useState([]);
-    const getDocSnap = async () => await getDoc(doc(db, "pages", 'resume')).then(doc => setResume(doc.data()))
+    const [gitHub, setGitHub] = useState([]);
+    const getDocSnap = async () => await getDoc(doc(db, "pages", 'resume')).then(doc => setResume(doc.data()));
+    const getGitHubSnap = async username => await fetch(`https://api.github.com/users/${username}`).then((data) => data.json())
+    .then((json) => setGitHub(json));
     useEffect(() => {
-      getDocSnap()
+      getGitHubSnap('mnichols08');
+      getDocSnap();
       }, []);
     return (
         <div className="resume">
-        <LeftSide resume={resume}/>
-        <RightSide resume={resume}/>
+        <LeftSide resume={resume} github={gitHub} />
+        <RightSide resume={resume} github={gitHub} />
     </div>
     );
   }
